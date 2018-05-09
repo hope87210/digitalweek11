5d7d05410e2356b0975d6239d321ae0b0ae61391
hope87210 <39110439+hope87210@users.noreply.github.com>
2018-05-09 11:42:34 +0800

    Add files via upload

---
+++ wk11.ino
@@ -0,0 +1,18 @@
+int LED_PIN1=13;
+int LED_PIN2=12;
+
+void setup() {
+  pinMode(LED_PIN1,OUTPUT);
+  pinMode(LED_PIN2,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_PIN1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(300);                       // wait for a second
+  digitalWrite(LED_PIN1, LOW);    // turn the LED off by making the voltage LOW
+  delay(300);                       // wait for a second
+  digitalWrite(LED_PIN2, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(300);                       // wait for a second
+  digitalWrite(LED_PIN2, LOW);    // turn the LED off by making the voltage LOW
+  delay(300);                       // wait for a second
+}
